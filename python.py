28240443fe05d2f3963c2a77754c0a972794c788
Hitesh <sangwanhitesh99@gamil.com>
2024-09-25 10:32:29 +0530

    update

---
+++ python.py
@@ -0,0 +1,19 @@
+# x=int(input("x is the greatest integer"))
+# y=int(input("y is the greatest integer"))
+# z=int(input("z is the greatest integer"))
+# if x>=y and x>=z:
+#     print("x is the greatest")
+# elif y>=x and y>=z:
+#     print("y is the greatest")
+# else:
+#     print("z is the greatest")
+# marks=4
+# if marks>=40:
+#     print("pass")
+# else:
+#     print("fail"
+#print("x is greatest function intege \ny is the smallest function integers")
+i=0
+while i<5:
+    print(i)
+    i+=1
